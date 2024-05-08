4e2de2660d3426403f5054461f6440990006e3a7
mackensey31712 <129702093+mackensey31712@users.noreply.github.com>
2024-05-08 02:37:52 +0000

    removed pygwalker

--- pages/2_Working_Hours.py
+++ pages/2_Working_Hours.py
@@ -6,7 +6,6 @@ import altair as alt
 from streamlit_lottie import st_lottie
 import requests
 import streamlit.components.v1 as components
-import pygwalker as pyg
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 from st_aggrid.shared import JsCode
 import plotly.express as px
